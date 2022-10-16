95ef548856da89bf1f950481b0643e60d58b9562
Fernando Ângelo <41452343+bastosfernando79@users.noreply.github.com>
2022-10-16 17:16:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const text = 'Eduardo é ruim pra caralho no Wild Rift!'
+
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+
+    index++;
+
+    if(index > text.length) {
+        index = 0;
+    }
+}
+
+setInterval(writeText, 100);
